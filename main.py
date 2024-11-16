c919abff06ac85d636ea8e179e390e8684fb4035
Paul Mena <paulmena07@outlook.com>
2024-11-16 12:57:17 -0500

    cleaned up functions in main

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ def assemble_and_run(program_file_path):
     assemble(program_file_path, 'machine_code.txt')
     run('machine_code.txt')
 
-def assemble_and_make_schem(program_file_path, schem_path, schem_file_name):
-    assemble(program_file_path, 'machine_code.txt')
-    create_schematic('machine_code.txt', schem_path, schem_file_name)
+def assemble_and_make_schem(program, path):
+    assemble('programs/' + program + '.as', 'machine_code.txt')
+    create_schematic('machine_code.txt', path, program)
 
-assemble_and_make_schem('programs/' + program + '.as', path, program)
+assemble_and_make_schem(program, path)
